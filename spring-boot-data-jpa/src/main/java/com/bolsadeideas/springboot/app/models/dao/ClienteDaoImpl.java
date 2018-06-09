49a79394b6a550d85d25680e3c7276ad888f27c7
arturoquiroz <37517709+arturoquiroz@users.noreply.github.com>
2018-06-08 19:08:16 -0500

    Add files via upload

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,50 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	//@Transactional(readOnly = true)
+	//Las anotaciones transactional solo son necesarias si no hay clase "service"
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+	@Override
+	//@Transactional
+	public void save(Cliente cliente) {
+		if(cliente.getId() != null && cliente.getId() > 0) {
+			em.merge(cliente);
+		}else {
+			em.persist(cliente);
+		}
+	}
+
+	@Override
+	//@Transactional(readOnly = true)
+	public Cliente findOne(Long id) {
+		return em.find(Cliente.class, id);
+	}
+
+	@Override
+	//@Transactional
+	public void delete(Long id) {
+		Cliente cliente = findOne(id);
+		em.remove(cliente);
+	}
+
+}
---
+++ spring-boot-data-jpa/target/classes/import.sql
@@ -0,0 +1,3 @@
+/*Populate tables*/
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (1, 'Juan', 'Perez', 'uncorreo.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, correo, create_at) VALUES (2, 'Jose', 'Mora', 'otrocorreo.com', '2017-09-10');
